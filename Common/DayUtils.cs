22f36bfb032b79b782ea45a2273c8eb0a8034b7d
Maurits Seelen <mauritsseelen@hotmail.com>
2024-12-05 11:06:10 +0100

    Get input as well

--- Common/DayUtils.cs
+++ Common/DayUtils.cs
@@ -6,18 +6,57 @@ public static class DayUtils
 {
     public static async Task Generate(int day, int year)
     {
+        await GenerateInputFile(day, year);
+        await GenerateClassFile(day, year);
+    }
+
+    private static async Task GenerateInputFile(int day, int year)
+    {
+        var currentDirectory = Directory.GetCurrentDirectory();
+        var inputFolderPath = Path.Combine(currentDirectory, $"Inputs/{year}");
+
+        if (!Directory.Exists(inputFolderPath))
+            Directory.CreateDirectory(inputFolderPath);
+
+        var inputFilePath = Path.Combine(inputFolderPath + $"/{day}.txt");
+        var aocInput = await GetAoCInput(day, year);
+
+        File.WriteAllText(inputFilePath, aocInput);
+    }
+
+    private static async Task<string> GetAoCInput(int day, int year)
+    {
+        var url = $"https://adventofcode.com/{year}/day/{day}/input";
+
+        using HttpClient client = new();
+        client.DefaultRequestHeaders.Add("Cookie", $"session={GetSessionCookie()}");
+
+        var response = await client.GetAsync(url);
+
+        return response.IsSuccessStatusCode
+            ? await response.Content.ReadAsStringAsync()
+            : throw new Exception($"Failed to fetch input: {response.ReasonPhrase}.");
+    }
+
+    private static string GetSessionCookie()
+    {
+        var currentDirectory = Directory.GetCurrentDirectory();
+        var filePath = Path.Combine(currentDirectory, "session.txt");
+
+        if (!File.Exists(filePath))
+            throw new FileNotFoundException("Session cookie file is not found.");
+
+        return File.ReadAllText(filePath);
+    }
+
+    private static async Task GenerateClassFile(int day, int year)
+    {
         var currentDirectory = Directory.GetCurrentDirectory();
         var templateFolderPath = Path.Combine(currentDirectory, "Common/Template");
         var filePath = Path.Combine(templateFolderPath, "day.txt");
 
         if (!File.Exists(filePath))
             throw new ArgumentException($"File {filePath} does not exist.");
-
-        var inputFolderPath = Path.Combine(currentDirectory, $"Inputs/{year}");
-        if (!Directory.Exists(inputFolderPath))
-            Directory.CreateDirectory(inputFolderPath);
-
-        File.Create(inputFolderPath + $"/{day}.txt");
 
         var content = await File.ReadAllTextAsync(filePath);
         content = content.Replace("[Year]", year.ToString());
